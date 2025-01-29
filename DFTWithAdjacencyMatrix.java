22d1a65adb6a8e5f79b855446740fcfcb2d1f8a8
Endala Harshavardhan <endalaharshavardhan@gmail.com>
2025-01-29 15:10:53 +0530

    DFTWithAdjacencyMatrix.java

---
+++ DFTWithAdjacencyMatrix.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DFTWithAdjacencyMatrix {
+
+    public static void DFS(int[][] adjacencyMatrix, int startVertex, boolean[] visited) {
+        visited[startVertex] = true;
+        System.out.print(startVertex + " ");
+
+        for (int i = 0; i < adjacencyMatrix.length; i++) {
+            if (adjacencyMatrix[startVertex][i] == 1 && !visited[i]) {
+                DFS(adjacencyMatrix, i, visited);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // Prompting the user for the number of vertices
+        System.out.print("Enter the number of vertices in the graph: ");
+        int vertices = scanner.nextInt();
+        
+        // Creating an adjacency matrix based on the number of vertices
+        int[][] graph = new int[vertices][vertices];
+        
+        // Getting the adjacency matrix input from the user
+        System.out.println("Enter the adjacency matrix (0 for no edge, 1 for edge): ");
+        for (int i = 0; i < vertices; i++) {
+            for (int j = 0; j < vertices; j++) {
+                graph[i][j] = scanner.nextInt();
+            }
+        }
+
+        boolean[] visited = new boolean[graph.length]; 
+        Arrays.fill(visited, false);
+
+        System.out.println("DFS Traversal:");
+        for (int i = 0; i < graph.length; i++) {
+            if (!visited[i]) {
+                DFS(graph, i, visited);
+            }
+        }
+
+        scanner.close();
+    }
+}
