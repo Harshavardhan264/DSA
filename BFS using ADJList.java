59fe2e2a51527c4a54fef346c6f13ad35920cef4
Endala Harshavardhan <endalaharshavardhan@gmail.com>
2025-03-26 16:02:27 +0530

    BFS using ADJ list

--- BFS using ADJList.java
+++ BFS using ADJList.java
@@ -1,65 +1,50 @@
 import java.util.*;
 
-public class BFSAdjList {
-    private int vertices;  
-    private LinkedList<Integer>[] adjList;  
-    public BFSAdjList(int vertices) {
-        this.vertices = vertices;
-        adjList = new LinkedList[vertices];
+class Graph {
+    private int V;
+    private LinkedList<Integer> adj[]; 
 
-        for (int i = 0; i < vertices; i++) {
-            adjList[i] = new LinkedList<>();
-        }
+    Graph(int v) {
+        V = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; ++i)
+            adj[i] = new LinkedList();
     }
-    public void addEdge(int src, int dest) {
-        adjList[src].add(dest);
-        adjList[dest].add(src); 
+
+    void addEdge(int v, int w) {
+        adj[v].add(w);
     }
-    public void BFS(int start) {
-        boolean[] visited = new boolean[vertices];  
-        Queue<Integer> queue = new LinkedList<>();  
 
-        visited[start] = true;
-        queue.add(start);
+    void BFS(int s) {
+        boolean visited[] = new boolean[V];
+        LinkedList<Integer> queue = new LinkedList<Integer>();
 
-        System.out.println("BFS traversal starting from vertex " + start + ":");
+        visited[s] = true;
+        queue.add(s);
 
-        while (!queue.isEmpty()) {
-            int vertex = queue.poll();
-            System.out.print(vertex + " ");
+        while (queue.size() != 0) {
+            s = queue.poll();
+            System.out.print(s + " ");
 
-            for (int neighbor : adjList[vertex]) {
-                if (!visited[neighbor]) {
-                    visited[neighbor] = true;
-                    queue.add(neighbor);
+            for (int n : adj[s]) {
+                if (!visited[n]) {
+                    visited[n] = true;
+                    queue.add(n);
                 }
             }
         }
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String args[]) {
+        Graph g = new Graph(4); 
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 0);
+        g.addEdge(2, 3);
+        g.addEdge(3, 3);
 
-        System.out.print("Enter the number of vertices: ");
-        int v = scanner.nextInt();
-
-        BFSAdjList graph = new BFSAdjList(v);
-
-        System.out.print("Enter the number of edges: ");
-        int e = scanner.nextInt();
-
-        System.out.println("Enter the edges (format: src dest): ");
-        for (int i = 0; i < e; i++) {
-            int src = scanner.nextInt();
-            int dest = scanner.nextInt();
-            graph.addEdge(src, dest);
-        }
-
-        System.out.print("Enter the starting vertex for BFS: ");
-        int start = scanner.nextInt();
-
-        graph.BFS(start);
-
-        scanner.close();
+        System.out.println("Following is Breadth First Traversal (starting from vertex 2)");
+        g.BFS(2);
     }
 }
